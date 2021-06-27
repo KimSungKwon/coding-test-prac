71bd6e3f72648df91355828849cd28e298a52d75
김성권 <39907207+KimSungKwon@users.noreply.github.com>
2021-06-28 00:03:32 +0900

    Create 3-2b.py

---
+++ Chapter03_Greedy/3-2b.py
@@ -0,0 +1,17 @@
+n, m, k = map(int, input().split())
+data = list(map(int, input().split()))
+data.sort(reverse=True)
+
+first = data[0]
+second = data[1]
+count = 0
+result = 0
+
+# first를 더하는 수
+count += int(m / (k + 1)) * k
+count += m % (k + 1)
+
+result += count * first
+result += (m - count) * second
+
+print(result)
