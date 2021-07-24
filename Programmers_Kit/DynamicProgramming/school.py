d4c46ba891fbe32b3b9a6f25c8e1742dd231594d
김성권 <39907207+KimSungKwon@users.noreply.github.com>
2021-07-24 16:51:51 +0900

    Create school.py

---
+++ Programmers_Kit/DynamicProgramming/school.py
@@ -0,0 +1,18 @@
+'''
+등굣길 
+https://programmers.co.kr/learn/courses/30/lessons/42898
+'''
+
+def solution(m, n, puddles):
+    # 지도 초기화
+    maps = [[0] * (m+1) for _ in range(n+1)]
+    
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if i == j == 1:
+                maps[i][j] = 1
+            elif [i, j] not in puddles:
+                maps[i][j] = maps[i-1][j] + maps[i][j-1]
+    answer = maps[i][j]
+    print(maps)
+    return answer
