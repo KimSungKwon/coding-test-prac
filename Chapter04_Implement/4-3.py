f9049ab3f00cd66071c234b07f8c805ae80f8fab
김성권 <39907207+KimSungKwon@users.noreply.github.com>
2021-11-19 00:01:43 +0900

    Update 4-3.py

--- Chapter04_Implement/4-3.py
+++ Chapter04_Implement/4-3.py
@@ -11,3 +11,24 @@ if int(n[1]) == 1 or int(n[1]) == 8:
   maximum -= 1
 
 print (maximum)
+
+# 또 다른 풀이
+# 방향 벡터 dx, dy 이용
+def solution(S):
+    x = (ord(S[0]) - ord('a') + 1)
+    y = int(S[1])
+    count = 0
+    #(y, x)
+    directX = [-1, 1, 2, 2, 1, -1, -2, -2]
+    directY = [-2, -2, -1, 1, 2, 2, 1, -1]
+     
+    for i in range(len(directX)):
+        dx = x + directX[i]
+        dy = y + directY[i]
+        if dx < 1 or dx > 8 or dy < 1 or dy > 8:
+            continue
+        count += 1
+        
+    return count
+
+print(solution( 'a1'  ))  # print 2
