4ecd91e3532c8ea3a8853ae96d69bd7199aabf89
김성권 <39907207+KimSungKwon@users.noreply.github.com>
2021-11-21 21:40:54 +0900

    Update 5-10.py

--- Chapter05_DFS_BFS/5-10.py
+++ Chapter05_DFS_BFS/5-10.py
@@ -24,7 +24,8 @@ def dfs(x, y):
 # 모든 경우의 수에 dfs 돌림
 for i in range(n):
     for j in range(m):
-        if dfs(i, j) == True:
+        if dfs(i, j) == True:   # 한번 dfs돌리면 연결된 노드들 전부 방문처리 되므로 방문처리 안 됐을때만 +1 하면 한 덩어리마다 
             result += 1
 
 print(result)
+
