8689f537e3593814d643e148ab631d7dad2c9c41
김성권 <39907207+KimSungKwon@users.noreply.github.com>
2022-01-18 23:26:39 +0900

    Update integer_triangle.py

--- Programmers_Kit/DynamicProgramming/integer_triangle.py
+++ Programmers_Kit/DynamicProgramming/integer_triangle.py
@@ -3,19 +3,25 @@
 https://programmers.co.kr/learn/courses/30/lessons/43105?language=python3
 
 메모이제이션은 안 쓰고 최적 부분 구조 성질 이용  Bottom up 방식
+
+모서리부분과 안쪽부분 구분지어서 점화식 계산
+
+triangle[i][j] = max(triangle[i-1][j-1]+triangle[i][j], triangle[i-1][j]+triangle[i][j])
 '''
 
 def solution(triangle):
+    answer = 0
+    length = len(triangle)
 
-    for i in range(1, len(triangle)):
+    for i in range(1, length):
         for j in range(0, len(triangle[i])):
             if j == 0:
-                triangle[i][j] += triangle[i-1][j]
+                triangle[i][j] = triangle[i-1][j] + triangle[i][j]
             elif j == len(triangle[i]) - 1:
-                triangle[i][j] += triangle[i-1][j-1]
+                triangle[i][j] = triangle[i-1][j-1] + triangle[i][j]
             else:
-                triangle[i][j] += max(triangle[i-1][j-1], triangle[i-1][j])
-    
-    answer = max(triangle[-1])
+                triangle[i][j] = max(triangle[i-1][j-1]+triangle[i][j], triangle[i-1][j]+triangle[i][j])
+        print(triangle[i])
 
+    answer = max(triangle[length-1])
     return answer
