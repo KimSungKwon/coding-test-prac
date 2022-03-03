5fa4418a04548692b0708355d8d77265fc6dbe0d
김성권 <39907207+KimSungKwon@users.noreply.github.com>
2022-03-04 00:07:26 +0900

    Create multiply_or_plus.py

---
+++ Chapter03_Greedy/multiply_or_plus.py
@@ -0,0 +1,16 @@
+'''
+Q 02
+곱하기 또는 더하기
+'''
+
+data = list(map(int, input()))
+
+result = data[0]
+
+for i in range(0, len(data)-1):
+    if data[i] == 0:
+        result += data[i+1]
+    else:
+        result *= data[i+1]
+        
+print(result)
