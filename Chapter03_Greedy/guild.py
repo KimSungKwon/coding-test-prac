a2e7b40d98ba8e2e87b249b26a18d31465a982d4
김성권 <39907207+KimSungKwon@users.noreply.github.com>
2022-03-03 23:51:14 +0900

    Create guild.py

---
+++ Chapter03_Greedy/guild.py
@@ -0,0 +1,19 @@
+'''
+모험가 길드
+'''
+n = int(input())  # 불 필요..
+
+data = list(map(int, input().split()))
+data.sort(reverse=True) # 그리디를 위해 정렬
+
+group = 0 # 여행을 떠날 수 있는 그룹 수
+while data:
+    top = data[0]
+    if top <= len(data):  # 필요한 모험가의 수가 남아있는 모험가의 수보다 적으면 
+        for i in range(top):
+            data.pop(0)
+        group += 1
+    else: # 필요한 모험가의 수가 남아있는 모험가의 수보다 크면
+        break
+
+print(group)
