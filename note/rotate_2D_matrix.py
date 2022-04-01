77e78769e21500f494cee89153695979c089de10
김성권 <39907207+KimSungKwon@users.noreply.github.com>
2022-04-01 23:19:02 +0900

    Create rotate_2D_matrix.py

---
+++ note/rotate_2D_matrix.py
@@ -0,0 +1,14 @@
+'''
+Rotate 2D matrix 90'
+'''
+
+def rotate_2D_matrix(arr):
+    n = len(matrix) # column length
+    m = len(arr[0]) # row length
+    result = [[0] * n for _ in range(m)]
+    
+    for i in range(n):
+      for j in range(m):
+        result[j][n - i - 1] = arr[i][j]
+    
+    return answer
