7d66beb2e605d3ddcb04b55fc1015cc20f4bba56
김성권 <39907207+KimSungKwon@users.noreply.github.com>
2022-08-11 23:06:47 +0900

    Update network.py

--- Programmers_Kit/DFS_BFS/network.py
+++ Programmers_Kit/DFS_BFS/network.py
@@ -25,9 +25,11 @@ def solution(n, computers):
                 if computers[temp][i] == 1 and visited[i] == False:
                     q.append(i)
    
-    for a in range(n):
-        if visited[a] == False:
-            bfs(a)
+    # bfs를 한번 실행하면 i노드로부터 연결되있는 모든 노드의 visited가 True가 되므로 
+    # bfs가 실행될 때(i노드가 미방문이면)마다 네트워크 수가 1  
+    for i in range(n):
+        if visited[i] == False:
+            bfs(i)
             answer += 1
     
     return answer
