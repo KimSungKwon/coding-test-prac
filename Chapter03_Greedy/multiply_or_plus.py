9f256be85c934130c74b7e2926a636288a3a200b
김성권 <39907207+KimSungKwon@users.noreply.github.com>
2022-09-22 22:59:41 +0900

    Update multiply_or_plus.py

--- Chapter03_Greedy/multiply_or_plus.py
+++ Chapter03_Greedy/multiply_or_plus.py
@@ -3,14 +3,13 @@ Q 02
 곱하기 또는 더하기
 '''
 
-data = list(map(int, input()))
+data = input()
 
-result = data[0]
-
-for i in range(0, len(data)-1):
-    if data[i] == 0:
-        result += data[i+1]
+answer = int(data[0])
+for i in range(len(data) - 1):
+    if data[i] == '0' or data[i + 1] == '0' or data[i] == '1' or data[i + 1] == '1':
+        answer += int(data[i + 1])
     else:
-        result *= data[i+1]
-        
-print(result)
+        answer *= int(data[i + 1])
+
+print(answer)
