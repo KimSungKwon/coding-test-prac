5a19b58f59b5bc51e18428217151c77c062e7e93
김성권 <39907207+KimSungKwon@users.noreply.github.com>
2022-12-30 00:15:28 +0900

    Update prime_number.py

--- Programmers_Kit/Brute_Force/prime_number.py
+++ Programmers_Kit/Brute_Force/prime_number.py
@@ -13,17 +13,23 @@ def solution(numbers):
     permut = []
     for r in range(len(numbers)):
         all = list(map(''.join, itertools.permutations(numbers, r+1)))  # 순열 생성
-        all = list(set(all))    # 중복 제거
+        all = list(set(all))    # 중복 제거 (01과 01 중복)
+        
         for j in range(len(all)):
             permut.append(int(all[j]))
-    permut = list(set(permut))  # 중복 제거
+            
+    permut = list(set(permut))  # 중복 제거 (01과 1 중복)
 
     answer = len(permut)
+    
+    # 소수 아닌거 갯수에서 
     for i in range(len(permut)):
         if permut[i] == 0 or permut[i] == 1:
             answer -= 1
+            
         for j in range(2, permut[i]):
             if permut[i] % j == 0:
                 answer -= 1
                 break
+                
     return answer
