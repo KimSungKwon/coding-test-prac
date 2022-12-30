64dbbccd61aa8ba1b838c6c9db0c8f582d2dbaad
김성권 <39907207+KimSungKwon@users.noreply.github.com>
2022-12-31 00:44:15 +0900

    Update carpet.py

--- Programmers_Kit/Brute_Force/carpet.py
+++ Programmers_Kit/Brute_Force/carpet.py
@@ -2,16 +2,19 @@
 카펫
 https://programmers.co.kr/learn/courses/30/lessons/42842
 
-yellow 값이 24라면, 24로 만들 수 있는 사각형은 24x1, 12x2, 8x3, 6x4
-각 종류마다 완전탐색.
-brown == (가로+2)x2 + (세로)x2 를 만족해야 함.
+brown == (가로+2)x2 + (세로)x2 를 만족하는 값을 완전탐색
 '''
-
 def solution(brown, yellow):
     answer = []
-    for i in range(1, yellow + 1):  # yellow가 24라면 1부터 24까지
+
+    for i in range(1, yellow + 1):
         if yellow % i == 0:
-            quot = yellow // i  # quot: 가로  i: 세로
-            if ((quot + 2) * 2) + (i * 2) == brown:  # brown == (가로+2)x2 + (세로+2)x2
-                answer = [quot+2, i+2]
-                return answer
+            height = i
+            width = yellow // i
+
+            if (width * 2) + ((height + 2) * 2) == brown:
+                answer.append(width + 2)
+                answer.append(height + 2)
+                break
+    
+    return answer
