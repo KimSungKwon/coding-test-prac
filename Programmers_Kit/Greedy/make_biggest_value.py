3a64c3553a5b687bbc6c13886132d1ffe03bc359
김성권 <39907207+KimSungKwon@users.noreply.github.com>
2023-01-03 00:17:29 +0900

    Update make_biggest_value.py

--- Programmers_Kit/Greedy/make_biggest_value.py
+++ Programmers_Kit/Greedy/make_biggest_value.py
@@ -8,16 +8,16 @@ while문을 빠져나가고 k가 1 이상일 경우 k값 만큼 pop
 '''
 
 def solution(number, k):
-    answer = ''
-    stack = []
+    answer = ''   
+    stack = []    # 스택에서 가장 큰 수를 만들어나감
 
-    for element in number:
-        while stack and stack[-1] < element and k != 0:
+    for element in number:   
+        while stack and stack[-1] < element and k != 0:    # 현재 값과 스택의 끝값을 비교해서 넣고 뺌
             stack.pop()
-            k -= 1
+            k -= 1    # 숫자 빼기 하나 소모
         stack.append(element)
 
-    while k > 0:  # k가 남아있을 경우
+    while k > 0:  # k가 남아있을 경우 스택의 가장 큰 수에서 맨 끝부터 뺴감
         stack.pop()
         k -= 1
 
